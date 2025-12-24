9f5eb0b8d5df87fa5773baeb97a5235e86cdec66
Thomas Duggan <duggantaw@gmail.com>
2025-12-24 17:16:36 -0330

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -6,18 +6,22 @@ shell.setAlias('c','clear')
 shell.setAlias('q','exit')
 shell.setAlias('h','help')
 shell.setAlias('man','help')
-shell.setAlias('r','reboot')
 shell.setAlias('e','edit')
 shell.setAlias('xed','edit')
 shell.setAlias('vim','edit')
 shell.setAlias('g','go')
 shell.setAlias('move','go')
+shell.setAlias('m','go')
 
 ---System Initialization
 shell.run('c')
 local id = os.getComputerID()
-print("Computer #"..id.." is now online.")
+print("Computer #"..id.." is now online")
 print("Please enter a label for Computer #"..id..":")
 local label = io.read()
-shell.run("label set \""..label.."\"")
+if label == "" then
+    print("Computer label not updated")
+else
+    shell.run("label set \""..label.."\"")
+end
 print("Setup complete!")
